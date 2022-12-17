1e8fbcce9f49e91d53d1a0ba7a59dbf47a881a02
Mohammed <iraqiProfessionalDesigner@gmail.com>
2022-12-17 21:04:58 +0700

    first commit

---
+++ src/app/App.js
@@ -0,0 +1,17 @@
+import '../styles/app.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <div className='circle-big'></div>
+        <div className='circle-small'></div>
+        <div className='rectangle-violet'></div>
+        <div className='rectangle'></div>
+        <div className='triangle'></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
